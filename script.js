6314fa1051ef85d54f977af58e8c9da952006728
King sama <211346423+King-sama312@users.noreply.github.com>
2025-07-15 15:26:12 +0530

    Add better version of both of previous functions and Add a determineWinner function

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
-function getComputerChoice() {
-    return Math.floor(Math.random() * 3)
-
+function GetComputerChoice() {
+    let a = Math.floor(Math.random() * 3)
+    if (a == 0) {
+        return "rock"
+    }
+    else if (a == 1) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
 }
 
-function getHumanChoice() {
-   let a = prompt("Choose rock, paper and scissors in small letters")
-   return a
+
+function GetHumanChoice() {
+    let userChoice = prompt("Choose rock, paper or scissors")
+    let ChoiceString = userChoice.toLowerCase()
+    if (ChoiceString !== "rock" && ChoiceString !== "paper" && ChoiceString !== "scissors") {
+        alert("Invalid choice! Please try again.")
+        return GetHumanChoice() // recursive retry
+    }
+
+    return ChoiceString
 }
 
-console.log("You chose", getHumanChoice())
+let computerChoice = GetComputerChoice()
+let humanChoice = GetHumanChoice()
 
-if (getComputerChoice() == 0) {
-    console.log("Computer chose Rock")
+console.log("You chose", humanChoice)
+console.log("Computer chose", computerChoice)
+
+function determineWinner() {
+    if (computerChoice == humanChoice) {
+        return "It's a tie!"
+    }
+    else if (humanChoice == "rock") {
+        if (computerChoice == "scissors") {
+            return "You win!"
+        }
+        else {
+            return "You lose!"
+        }
+    }
+    else if (humanChoice == "scissors") {
+        if (computerChoice == "paper") {
+            return "You win!"
+        }
+        else {
+            return "You lose!"
+        }
+    }
+    else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            return "You win!"
+        }
+        else {
+            return "You lose!"
+        }
+    }
 }
-else if (getComputerChoice() == 1) {
-    console.log("Computer chose Paper")
-}
-else {
-    console.log("Computer chose Scissors")
-}
+
+console.log(determineWinner())
+
